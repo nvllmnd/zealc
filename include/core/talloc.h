5537fcf9183b086070f9f6d10d759acea2a58a12
nvllmnd <nvllmnd@pm.me>
2026-05-26 13:17:40 -0700

    interim

--- include/core/talloc.h
+++ include/core/talloc.h
@@ -1,21 +1,40 @@
+//!
+//! This header contains functions for interacting with a few static instances of temporary allocators,
+//! each used for a different purpose. The String and Array pads are great for building arrays and strings with dynamic
+//! length at runtime, before copying them to a permanent location, for instance an [Arena]. in contrast to this,
+//! pushing elements into a vec allocated by an [Arena] beyond their capacity, requires copying the entire vec into a
+//! bigger allocation, still allocated by [Arena], causing spurrious copies that leak memory, the [Arena]
+//! allocator handles reallocation by leaking the old contents, as it has no mechanism to free memory beyond resetting
+//! the entire [Arena]
+//!
+//! Temp allocators used:
+//!     A) TempAllocator (talloc_*) - Used for general temporary allocations, frequently cleared, pointered returned by
+//!     this allocator should not live for very long each generation, also contains functions to scope allocations
+//!
+//!     B) StringPad (spad_*) - Used for building strings of dynamic length, has functions to format strings in a printf
+//!     style, as well as copy the built string into a buffer or [Allocator].
 #pragma once
 
 #include "nv.h"
-#include "nv/core/attributes.h"
-#include "nv/core/constants.h"
 
 // ##########################################################
 // ################## Temp Allocator ########################
 // ##########################################################
 
+/// @brief Default [VirtMemOpts] passed to either [talloc_init], [spad_init], or [tpad_init] if caller does not
+/// call it themselves before invoking any other functions associated with that temp allocator
 static constexpr const VirtMemOpts TALLOC_DEFAULT_OPTS =
     (VirtMemOpts){.size_in_mb = 1024 * 24, .initial_commit = KILOBYTES(4)};
+
+
+/// @brief Number of calls to talloc_scope_begin/talloc_scope_end call pairs allowed
+static constexpr const i32 TALLOC_MAX_SCOPE_DEPTH = 128;
 
 /// @brief Allocates virtual memory for a static instance of a temporary Allocator
 /// @warn Temp Allocator is NOT thread safe. only safe to call write from main thread
 /// It is safe to skip calling this if you are okay with the temp allocator being initialized with [TALLOC_DEFAULT_OPTS]
-/// @param() - [VirtMemOpts] Forwarded to initialize Temp Allocators backing [VirtMem]
-void talloc_init(VirtMemOpts opts);
+/// @param(i32 vmem_size_mb) - Number of megabytes to initialize Temp Allocators backing [VirtMem]. 
+void talloc_init(i32 vmem_size_mb);
 
 /// @brief Releases virtual memory used by temporary alloctor back to the operating system.
 void talloc_destroy(void);
@@ -23,44 +42,32 @@ void talloc_destroy(void);
 /// resets used counter back to 0. This operation is done in O(1)
 void talloc_clear(void);
 
-// void talloc_array_begin_(i32 elem_size);
-// void talloc_array_end_(void);
-
-// #define talloc_array_begin(T) (talloc_array_begin_(sizeof(T)))
-
-// #define talloc_array_end() (talloc_array_end_())
-
-// void talloc_string_begin(void);
-
-// void* talloc_array_push_(MemLayout layout);
-
-// #define talloc_array_push(v)                              \
-//   ({                                                      \
-//     void* _p = talloc_array_push_(sizeof(__typeof((v)))); \
-//     memcpy(_p, &(v), sizeof(__typeof((v))))               \
-//   })
-
-// char talloc_putchar(char c);
-// u8 talloc_putbyte(u8 b);
-
 RETURNS_NON_NULL
 void* talloc_allocate(MemLayout layout);
 
 RETURNS_NON_NULL
 void* talloc_zallocate(MemLayout layout);
 
-// i32 talloc_array_len(void);
-
-// PARAMS_NONNULL(1)
-// const void* talloc_as_array_(i32* len_out);
-
-// #define talloc_as_array(T, _len_out) ((__typeof(T)*)talloc_as_array((_len_out)))
-
-#define talloc_allocate_tp(T) ((__typeof(T)*)talloc_allocate(mlayout_new(T)))
+#define talloc_allocate_tp(T) ((__typeof(T)*)talloc_allocate(sizeof(__typeof(T))))
+#define talloc_zallocate_tp(T) ((__typeof(T)*)talloc_zallocate(sizeof(__typeof(T))))
 
 #define talloc_allocate_array(T, N) ((__typeof(T)*)talloc_allocate(mlayout_array(T, N)))
+#define talloc_allocate_vec(T, _n) ((__typeof(T)*)talloc_allocate(mlayout_vec(T, (_n))))
 
-// bool talloc_is_init(void);
+#define talloc_zallocate_array(T, N) ((__typeof(T)*)talloc_zallocate(mlayout_array(T, N)))
+#define talloc_zallocate_vec(T, _n) ((__typeof(T)*)talloc_zallocate(mlayout_vec(T, (_n))))
+
+/// @brief Begins a temporary allocations scope
+/// @details Pushes a new scope onto TempAlloc's inner scope stack. There should be a matching call to [talloc_scope_end] for every call to this function
+/// These 2 functions can allow you to easily reuse sections of memory through scopes, instead of blindly clearing and reusing
+/// @warning If this function is called more than [TALLOC_MAX_SCOPE_DEPTH] at a time, (without closing any scopes), [abort] is called and execution terminates
+void talloc_scope_begin(void);
+
+
+/// @brief Ends a temporary allocations scope
+/// @details Pops scope from the back of TempAlloc's inner scope stack. There should be a matching call to [talloc_scope_begin] for every call to this function
+/// These 2 functions can allow you to easily reuse sections of memory through scopes, instead of blindly clearing and reusing
+void talloc_scope_end(void);
 
 // ##########################################################
 // ##############  String Builder (String Pad) ##############
@@ -87,8 +94,9 @@ i32 spad_clone_into(char* buff_out, i32 buff_len);
 
 /// @brief duplicates currently built string using given allocator
 /// @param(Allocator alloc) - Used to duplicate Spad's inner string buffer
-/// @returns null-terminated c-string pointing to duplicated  string in given allocator
-const char* spad_dup_in(Allocator alloc);
+/// @returns null-terminated string slice pointing to where the cloned string lives in given allocator. slice length
+/// does not include null terminating character
+sslice spad_clone_string(Allocator alloc);
 
 sslice spad_nappend(const char* s, i32 len);
 sslice spad_append(const char* s);
@@ -120,9 +128,3 @@ sslice spad_fappend(const char* fmt, ...);
 /// variadic arguments.
 sslice spad_vfappend(const char* fmt, char delim, va_list args);
 
-// ##########################################################
-// ##############  Array Builder (Type Pad) #################
-// ##########################################################
-
-void tpad_init(VirtMemOpts opts);
-void tpad_destroy(void);
